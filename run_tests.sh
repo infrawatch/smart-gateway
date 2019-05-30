25e6c16f21b22d71a9f59df933ffc51b3efdd440
Martin Magr <mmagr@redhat.com>
2019-05-30 16:22:17 +0200

    Move the whole test suite to centos
    Moves also vet, lint and coverage analysis to centos container because
    some of it does not work on ubuntu due to invalid qpid libs.

--- run_tests.sh
+++ run_tests.sh
@@ -9,11 +9,23 @@ export PATH=$PATH:$GOPATH/bin
 # get dependencies
 yum install -y epel-release
 yum install -y golang qpid-proton-c-devel iproute
+go get -u golang.org/x/tools/cmd/cover
+go get -u github.com/mattn/goveralls
+go get -u golang.org/x/lint/golint
 go get -u github.com/golang/dep
 sh $GOPATH/src/github.com/golang/dep/install.sh
 
-ss -lnp
-ss -ntp
 # run test suite
 dep ensure -v --vendor-only
 go test -v -timeout=10s ./tests/*
+
+# run lints
+go vet ./cmd/main.go
+golint . | xargs -r false
+
+# analyze test coverage
+# works correctly only with Go-1.11+ due to: https://github.com/golang/go/issues/25093
+echo "mode: set" > coverage.out
+for pkg in $(go list ./internal/pkg/...); do go test -cover -coverpkg $pkg -coverprofile coverfragment.out ./tests/internal_pkg/* && grep -h -v "mode: set" coverfragment.out >> coverage.out; done
+#TODO(mmagr): Uncomment submission by straight push to master as secured env is not correctly unecrytpted in PR
+#$GOPATH/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
