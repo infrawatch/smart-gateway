6afab9ece9bdb0e5b0928b100b77acfb0262cbbd
Leif Madsen <lmadsen@redhat.com>
2019-10-04 00:33:22 -0400

    Enable go test

--- run_tests.sh
+++ run_tests.sh
@@ -22,13 +22,10 @@ echo " *** Running pre-commit code validation"
 # run unit tests
 echo " *** Running test suite"
 # TODO: re-enable the test suite once supporting changes result in tests to pass
-#go test -v ./...
+go test -v ./...
 
 # check test coverage
 echo " *** Running code coverage tooling"
-
-# TODO: remove this set +e once all our tests are passing without issue
-set +e
 
 echo "mode: set" > coverage.out
 for pkg in $(go list ./internal/pkg/...)
