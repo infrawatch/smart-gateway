3cbe198fff568aca9738877ad15b93da230fb388
Aneesh Puttur <aneeshputtur@gmail.com>
2020-05-05 16:45:35 -0400

    Fixed panic when parsing events for alerts

--- internal/pkg/events/incoming/collectd.go
+++ internal/pkg/events/incoming/collectd.go
@@ -88,6 +88,11 @@ func (evt *CollectdEvent) ParseEvent(data string) error {
 
 //assimilateMap recursively saves content of the given map to destination map of strings
 func assimilateMap(theMap map[string]interface{}, destination *map[string]string) {
+	defer func() { //recover from any panic
+		if r := recover(); r != nil {
+			log.Printf("Panic:recovered in assimilateMap %v\n", r)
+		}
+	}()
 	for key, val := range theMap {
 		switch value := val.(type) {
 		case map[string]interface{}:
@@ -102,6 +107,10 @@ func assimilateMap(theMap map[string]interface{}, destination *map[string]string
 				}
 			}
 			(*destination)[key] = strings.Join(aList, ",")
+		case float64, float32:
+			(*destination)[key] = fmt.Sprintf("%f", value)
+		case int:
+			(*destination)[key] = fmt.Sprintf("%d", value)
 		default:
 			// assimilate KV pair
 			(*destination)[key] = value.(string)
